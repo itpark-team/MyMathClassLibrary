45c17ac8f56bc6f11f497ddaeb246f538b4795ec
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-02-05 12:12:07 +0300

    Добавьте файлы проекта.

---
+++ MyMathClassLibrary/PlainArithmetic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathClassLibrary
+{
+    public class PlainArithmetic
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Dev(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("devision by zero");
+            }
+            return a / b;
+        }
+
+    }
+}
